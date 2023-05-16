9da44901ad602698ba54c76d4a92aaa95b7cb930
AffanImran1303 <115869839+AffanImran1303@users.noreply.github.com>
2023-05-16 13:42:29 +0500

    lottery.sol

--- lottery.sol
+++ lottery.sol
@@ -1,42 +1,33 @@
-//SPDX-License-Identifier:UNLICENSED
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+contract Lottery{
+//Array of Address
+address payable[] public players;
+address public manager;
+address payable public winner;
 
-contract lottery{
-    //OWNER-who runs the lottery
-    address public owner;
-    //PLAYERS-who will participate in the lottery
-    address payable[] public players;
-    //
+constructor(){
+manager=msg.sender;
+}
+function participate()public payable{
+require(msg.value==3 ether,"Ether is not sufficient");
+players.push(payable(msg.sender));
+}
+function getBalance()public view returns(uint){
+require(manager==msg.sender,"You are not manager!");
+return address(this).balance;
+}
+function random() public view returns(uint){
+return uint (keccak256(abi.encodePacked(block.difficulty,block.timestamp,players.length)));
+}
+function pickWinner()public{
+require(manager==msg.sender);
+require(players.length>=3);
+uint r=random();
+uint index=r%players.length;
+winner=players[index];
+winner.transfer(getBalance());
+players=new address payable[](0);
+}
 
-    //At the time of deploy constructor will only run once.
-    constructor()
-    {
-        owner=msg.sender;
-    }
-    //Receiving the Minimum Amount(0.1ETH) from the players.
-    receive() external payable{
-        require(msg.value ==0.1 ether);
-        players.push(payable(msg.sender));
-    } 
-    //Function to get the total balance of lottery.
-    function lottery_Balance() public view returns(uint)
-    {   
-        require(msg.sender==owner);
-        return address(this).balance;
-    }
-    //Function to generate the random number(index) of the player list.
-    function random_player_id() public view returns(uint)
-    {
-      return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
-    }
-    //Function to select the winner of lottery.
-    function selecting_winner() public 
-    {
-        require(msg.sender==owner);
-        require(players.length>=3);
-        uint player_id = random_player_id()%players.length;
-        address payable winner;
-        winner = players[player_id];
-        winner.transfer(lottery_Balance());
-    }
 }
